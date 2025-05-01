17c7dc10896f2c4a7eed5f437ff96a4337b28e42
Aidan Foster <aidanf@nvidia.com>
2025-04-30 18:51:13 -0700

    Move override

--- docs/conf.py
+++ docs/conf.py
@@ -65,7 +65,6 @@ myst_title_to_header = True
 
 html_theme = "sphinx_rtd_theme"
 html_title = "Slang Documentation"
-html_static_path = ['__static']
 html_css_files = ["theme_overrides.css"]
 html_theme_options = {
     "light_css_variables": {
